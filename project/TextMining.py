c7f9ddf4b1efe2725a47a2152da12a7fbd2287a2
KatDestroyer <elrodkathryn@gmail.com>
2022-04-11 13:03:59 +0200

    improved ux

--- project/TextMining.py
+++ project/TextMining.py
@@ -1,5 +1,6 @@
 import requests
 import re
+import os
 import numpy
 import spacy
 nlp = spacy.load('en_core_web_sm')
@@ -120,45 +121,6 @@ def sentiment_to_line(value, min=-1, max=1):
     pos = int(numpy.round(pos*len(line)))
     line = line[:pos]+'*'+(line[(pos+1):] if pos<len(line) else '')
     return '(-) ['+line+'] (+)'
-
-"""
-#code below is to implement VADER with NLTK. Doesn't work yet though.    
-def run_spacey(textual_unit, 
-              lemmatize=False, 
-              parts_of_speech_to_consider=None,
-              verbose=False):
-
-    doc = nlp(textual_unit)
-        
-    input_to_vader = []
-
-    for sent in doc.sents:
-        for token in sent:
-
-            to_add = token.text
-
-            if lemmatize:
-                to_add = token.lemma_
-
-                if to_add == '-PRON-': 
-                    to_add = token.text
-
-            if parts_of_speech_to_consider:
-                if token.pos_ in parts_of_speech_to_consider:
-                    input_to_vader.append(to_add) 
-            else:
-                input_to_vader.append(to_add)
-
-    scores = vader_model.polarity_scores(' '.join(input_to_vader))
-    
-    if verbose:
-        print()
-        print('INPUT SENTENCE', sent)
-        print('INPUT TO VADER', input_to_vader)
-        print('VADER OUTPUT', scores)
-
-    return scores['compound']
-"""
     
 def sentiment(text):
     scores = vader_model.polarity_scores(text)
@@ -171,46 +133,34 @@ print("""Welcome to
  / /_/ / /_/ / /  / /_/ / / /    | |/ / /_/ / /_/ /  __/ /    
 /_____/\__,_/_/   \__/_/ /_/     |___/\__,_/\__,_/\___/_/     """)
 while True:
-    print("""
-What would you like to do?
-[1] Analyize a topic
-[2] Analyze tweet replies
-[3] Analyze arbitrary text
-[0] Exit""")
-    mode = str(input('=> '))
+    query = str(input('\nEnter a topic, URL, or filename to analyze: '))
+    match = re.match(r'(?:https?:\/\/)?(?:www.)?twitter.com/[^/]*/status/([0-9]*)', query)
+    if match:
+        num_r = input('How many reply tweets to get? (10-100, default=25) ')
+        num_r = num_r if num_r else 25
 
-    if mode=='1':
-        topic = '"'+input('Enter a topic to analyze: ').strip()+'"'
+        print('Analyzing replies...')
+        tweets = get_replies(match.group(1), bearer_token, num_r)
+        snt = analyze_tweets(tweets, num_replies=0)
+    elif os.path.isfile(query):
+        with open(query) as file:
+            text = file.read()
+        doc = nlp(text)
+        snt = numpy.average([sentiment(str(s)) for s in doc.sents])
+    else:
+        if not query[0] == '%':
+            query = '"'+query+'"'
+        else:
+            query = query[1:]
+
         num_p = input('How many parent tweets to get? (10-100, default=100) ')
         num_p = num_p if num_p else 100
         num_r = input('How many reply tweets to get? (10-100, default=0) ')
         num_r = num_r if num_r else 0
 
         print('Analyzing topic...')
-        tweets = get_tweets(topic, bearer_token, num_p)
+        tweets = get_tweets(query, bearer_token, num_p)
         snt = analyze_tweets(tweets, num_replies=num_r)
-    elif mode=='2':
-        match = None
-        while not match:
-            parent = input('Enter URL of parent tweet: ')
-            match = re.match(r'(?:https?:\/\/)?(?:www.)?twitter.com/[^/]*/status/([0-9]*)', parent)
-            if not match:
-                print('Error: Not a valid tweet URL')
-        conversation_id = match.group(1)
-        num_r = input('How many reply tweets to get? (10-100, default=25) ')
-        num_r = num_r if num_r else 25
-
-        print('Analyzing replies...')
-        tweets = get_replies(conversation_id, bearer_token, num_r)
-        snt = analyze_tweets(tweets, num_replies=0)
-    elif mode=='3':
-        filename = input('Enter path to file: ')
-        with open(filename) as file:
-            text = file.read()
-        doc = nlp(text)
-        snt = numpy.average([sentiment(str(s)) for s in doc.sents])
-    else:
-        break
 
     if snt:
         print('Done! Found overall ' + sentiment_to_sentence(snt) + ' sentiment (' + str(numpy.round(snt, 4))+')')
